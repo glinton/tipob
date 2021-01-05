abace89462125e4f647ac6e46c55704a1fce27dd
greg linton <greg@influxdata.com>
2021-01-04 18:16:36 -0700

    Allow fast actions, handle all threads

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import wave
 from enum   import Enum
 from random import randrange
 
+CHUNK = 1024
+
 # Get single character from keyboard without pressing enter.
 class _Getch:
     """Gets a single character from standard input.  Does not echo to the screen."""
@@ -20,7 +22,6 @@ class _Getch:
             self.impl = _GetchWindows()
 
     def __call__(self): return self.impl()
-
 
 class _GetchUnix:
     def __init__(self):
@@ -37,7 +38,6 @@ class _GetchUnix:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
 
-
 class _GetchWindows:
     def __init__(self):
         import msvcrt
@@ -46,21 +46,22 @@ class _GetchWindows:
         import msvcrt
         return msvcrt.getch()
 
-
 getch = _Getch()
 
-def playAudio(f, stream, num):
+def playAudio(f, stream, num, stop):
     for x in range(0, num):
-        data = f.readframes(chunk)
+        data = f.readframes(CHUNK)
 
         while data:
             stream.write(data)
-            data = f.readframes(chunk)
+            data = f.readframes(CHUNK)
 
         f.rewind()
 
+        if stop():
+            break
 
-def playSound(filename, num):
+def playSound(filename, num, stop):
     f = wave.open(filename, "rb")
 
     p = pyaudio.PyAudio()
@@ -70,7 +71,7 @@ def playSound(filename, num):
                     rate = f.getframerate(),
                     output = True)
 
-    playAudio(f, stream, num)
+    playAudio(f, stream, num, stop)
 
     #stop stream
     stream.stop_stream()
@@ -80,60 +81,34 @@ def playSound(filename, num):
     p.terminate()
 
 def playBackgroundSound(filename, num):
-    BackgroundAudio(playSound(filename, num)).start()
-    # thread = BackgroundAudio(playSound(filename, num)).start()
-    # thread.start()
+    t = threading.Thread(target=playSound, args=[filename, num, lambda: False])
+    t.start()
 
-
-# For playing audio without blocking the main thread
-class BackgroundAudio(threading.Thread):
-
-    def __init__(self, f):
-        threading.Thread.__init__(self)
-        self.daemon = True
-        self.runner = f
-
-    def run(self):
-        self.runner()
-
-def playBackground():
-    filename1 = './audio/filler/Background-1.wav'
-    playSound(filename1,99)
-    # threading.Thread(target=playSound(filename1, 99), name="BackgroundMusic").start()
+    return t
 
 class Prompt(Enum):
     Twist = 0
     Pull = 1
     Bop = 2
 
-def promptRandom():
-    prompt = Prompt(randrange(3))
-
-    playSound('./audio/prompt/'+prompt.name+'.wav', 1)
-    # playBackgroundSound('./audio/prompt/'+prompt.name+'.wav', 1)
-    # BackgroundAudio(playSound('./audio/prompt/'+prompt.name+'.wav', 1)).start()
+def prompt(promptName):
+    t = playBackgroundSound('./audio/prompt/'+promptName+'.wav', 1)
 
     # todo: add timeout
-    if getch() == prompt.name[0].lower():
-        playSound('./audio/success/'+prompt.name+'.wav',1)
-        # playBackgroundSound('./audio/success/'+prompt.name+'.wav',1)
-        return True
-    
-    return False
-    # else:
-    #     # todo: randomize
-    #     playSound('./audio/fail/TryAgain.wav',1)
+    if getch() == promptName[0].lower():
+        t.join()
 
+        playSound('./audio/success/'+promptName+'.wav', 1, lambda: False)
+        return True
+
+    t.join()
+    return False
 
 # starting - press button (key) to begin
 print("Press enter to begin")
 
 # wait for user to start
 input()
-
-filename2 = './audio/filler/Background-2.wav'
-filename3 = './audio/filler/Background-3.wav'
-chunk = 1024
 
 # initial seconds between prompts
 interval = 2.0
@@ -143,22 +118,41 @@ decrementor = 0.2
 winSpeed = 5
 
 # start background music
-BackgroundAudio(playBackground).start()
-
-# playBackgroundSound(playSound(filename2, 99))
+bgSwap = False
+bgSound = threading.Thread(target=playSound, args=['./audio/filler/Background-'+str(randrange(13))+'.wav', 99, lambda: bgSwap])
+bgSound.start()
 
 win = True
-i = 0
+wins = 0
 while win:
     time.sleep(interval)
-    win = promptRandom()
-    i += 1
-    if interval >= 0.2 and i % winSpeed == 0:
-        interval-=decrementor
+
+    # random prompt actions
+    win = prompt(Prompt(randrange(3)).name)
+    wins += 1
+
+    if win == True and wins % winSpeed == 0:
+        if interval >= 0.2:
+            interval-=decrementor
+
+        # start new sound
+        bgSwap = True
+        bgSound.join()
+        bgSwap = False
+
+        playSound('./audio/ShiftGear.wav', 1, lambda: False)
+
+        bgSound = threading.Thread(target=playSound, args=['./audio/filler/Background-'+str(randrange(11)+1)+'.wav', 99, lambda: bgSwap])
+        bgSound.start()
 
 if win == False:
-    playSound('./audio/fail/TryAgain.wav',1)
+    # end background sound
+    bgSwap = True
+    bgSound.join()
 
-# background audio for diminishing x seconds 2seconds -.25 seconds
-# random prompt actions
+    # todo: randomize failure
+    playSound('./audio/fail/TryAgain.wav', 1, lambda: False)
+
+print("You succeeded", wins-1, "times!")
+
 # success or fail - record to influx
